64d77e43b99db295dd9f310981efedd91e291398
tokitsu-kaze <861794979@qq.com>
2018-12-03 23:16:48 +0800

    Update kruskal.cpp

--- 图论/kruskal.cpp
+++ 图论/kruskal.cpp
@@ -1,39 +1,45 @@
-//kruskal
-int pre[MAX],ans;
+struct dsu
+{
+	int pre[MAX];
+	void init(int n)
+	{
+		int i;
+		for(i=1;i<=n;i++) pre[i]=i;
+	}
+	int find(int x)
+	{
+		if(pre[x]!=x) pre[x]=find(pre[x]);
+		return pre[x];
+	}
+	bool merge(int a,int b)
+	{
+		int ra,rb;
+		ra=find(a);
+		rb=find(b);
+		if(ra!=rb)
+		{
+			pre[ra]=rb;
+			return 1;
+		}
+		return 0;
+	}
+}dsu;
 struct node
 {
-	int x,y,v;
+	int x,y,w;
 	node(){}
-	node(int a,int b,int c):x(a),y(b),v(c){}
-	friend bool operator<(node a,node b)
+	node(int a,int b,int c):x(a),y(b),w(c){}
+	friend bool operator<(node a,node b) {return a.w<b.w;}
+};
+vector<node> edge;
+int kruskal(int n)
+{
+	int res=0;
+	dsu.init(n);
+	sort(all(edge));
+	for(int i=0;i<sz(edge);i++)
 	{
-		return a.v<b.v;
+		if(dsu.merge(edge[i].x,edge[i].y)) res+=edge[i].w;
 	}
-}a[MAX];
-void init(int n)
-{
-	for(int i=1;i<=n;i++) pre[i]=i;
+	return res;
 }
-int find(int x)
-{
-	if(pre[x]!=x) pre[x]=find(pre[x]);
-	return pre[x];
-}
-void merge(node s)
-{
-	int rx,ry;
-	rx=find(s.x);
-	ry=find(s.y);
-	if(rx!=ry)
-	{
-		pre[rx]=ry;
-		ans+=s.v;
-	}
-}
-void kruskal(int n,int m,node *s)
-{
-	init(n);
-	ans=0; 
-	sort(s,s+m);
-	for(int i=0;i<m;i++) merge(s[i]);
-} 
