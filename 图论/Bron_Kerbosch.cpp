b81e242a3f9f9a0b25a28dc50d0820552684f758
tokitsu-kaze <861794979@qq.com>
2019-01-21 21:41:08 +0800

    Create Bron_Kerbosch.cpp

---
+++ 图论/Bron_Kerbosch.cpp
@@ -0,0 +1,50 @@
+struct Bron_Kerbosch
+{
+	static const int N=;
+	bitset<N> MASK,ZERO,mp[N];
+	int n,cnt_clique;
+	void init(int _n)
+	{
+		n=_n;
+		for(int i=0;i<=n;i++) mp[i].reset();
+		ZERO.reset();
+		MASK=ZERO;
+		MASK.flip();
+	}
+	void add_edge(int a,int b) //0~n-1 , undir
+	{
+		if(a==b) return;
+		mp[a][b]=mp[b][a]=1;
+	}
+	void dfs(bitset<N> now,bitset<N> some,bitset<N> none)
+	{
+		if(some.none()&&none.none())//one max clique
+		{
+			cnt_clique++;
+			return;
+		}
+		bitset<N> r=some;
+		bool fi=1;
+		for(int i=0;i<n;i++)
+		{
+			if(!r[i]) continue;
+			if(fi)
+			{
+				fi=0;
+				r&=mp[i]^MASK;
+			}
+			now[i]=1;
+			dfs(now,some&mp[i],none&mp[i]);
+			now[i]=0;
+			some[i]=0;
+			none[i]=1;
+		}
+	}
+	int count_clique()
+	{
+		cnt_clique=0;
+		bitset<N> now;
+		dfs(now,MASK,ZERO);
+		return cnt_clique;
+	}
+}bk;
