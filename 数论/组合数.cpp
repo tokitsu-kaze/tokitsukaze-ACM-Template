0fce118f40033d9232dc30db75f624c6e3c316e6
tokitsu-kaze <861794979@qq.com>
2019-07-23 20:24:53 +0800

    Update 组合数.cpp

--- 数论/组合数.cpp
+++ 数论/组合数.cpp
@@ -15,29 +15,28 @@ void init(int n)
 
 
 //2.´ó·¶Î§ 
-ll pow2(ll a,ll b)
+int pow2(int a,int b)
 {
-	ll res=1;
+	int res=1;
 	while(b)
 	{
-		if(b&1) res=res*a%mod;
-		a=a*a%mod;
+		if(b&1) res=1ll*res*a%mod;
+		a=1ll*a*a%mod;
 		b>>=1;
 	}
 	return res;
 }
-ll inv(ll x){return pow2(x,mod-2);}
-ll fac[MAX],invfac[MAX];
+int inv(int x){return pow2(x,mod-2);}
+int fac[MAX],invfac[MAX];
 void init(int n)
 {
-	ll i;
 	fac[0]=1;
-	for(i=1;i<=n;i++) fac[i]=fac[i-1]*i%mod;
+	for(int i=1;i<=n;i++) fac[i]=1ll*fac[i-1]*i%mod;
 	invfac[n]=inv(fac[n]);
-	for(i=n-1;~i;i--) invfac[i]=invfac[i+1]*(i+1)%mod;
+	for(int i=n-1;~i;i--) invfac[i]=1ll*invfac[i+1]*(i+1)%mod;
 }
-ll C(int n,int m)
+int C(int n,int m)
 {
 	if(m>n||m<0||n<0) return 0;
-	return fac[n]*invfac[m]%mod*invfac[n-m]%mod;
+	return 1ll*fac[n]*invfac[m]%mod*invfac[n-m]%mod;
 }
