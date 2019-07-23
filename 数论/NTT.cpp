18421c0a72230be2e78a2328b9042c88b316963d
tokitsu-kaze <861794979@qq.com>
2019-07-23 20:24:50 +0800

    Update NTT.cpp

--- 数论/NTT.cpp
+++ 数论/NTT.cpp
@@ -1,16 +1,15 @@
-//ntt
 namespace NTT
 {
-	const ll g=3;
-	const ll p=998244353;
-	ll wn[35];
-	ll pow2(ll a,ll b)
+	const int g=3;
+	const int p=998244353;
+	int wn[35];
+	int pow2(int a,int b)
 	{
-		ll res=1;
+		int res=1;
 		while(b)
 		{
-			if(b&1) res=res*a%p;
-			a=a*a%p;
+			if(b&1) res=1ll*res*a%p;
+			a=1ll*a*a%p;
 			b>>=1;
 		}
 		return res;
@@ -19,9 +18,9 @@ namespace NTT
 	{
 		for(int i=0;i<25;i++) wn[i]=pow2(g,(p-1)/(1LL<<i));
 	}
-	void ntt(VL &a,int len,int f)
+	void ntt(VI &a,int len,int f)
 	{
-		ll i,j=0,t,k,w,id;
+		int i,j=0,t,k,w,id;
 		for(i=1;i<len-1;i++)
 		{
 			for(t=len;j^=t>>=1,~j&t;);
@@ -32,30 +31,49 @@ namespace NTT
 			t=i<<1;
 			for(j=0;j<len;j+=t)
 			{
-				for(k=0,w=1;k<i;k++,w=w*wn[id]%p)
+				for(k=0,w=1;k<i;k++,w=1ll*w*wn[id]%p)
 				{
-					ll x=a[j+k],y=w*a[j+k+i]%p;
-					a[j+k]=(x+y)%p;
-					a[j+k+i]=(x-y+p)%p;
+					int x=a[j+k],y=1ll*w*a[j+k+i]%p;
+					a[j+k]=x+y;
+					if(a[j+k]>=p) a[j+k]-=p;
+					a[j+k+i]=x-y;
+					if(a[j+k+i]<0) a[j+k+i]+=p;
 				}
 			}
 		}
 		if(f)
 		{
 			for(i=1,j=len-1;i<j;i++,j--) swap(a[i],a[j]);
-			ll inv=pow2(len,p-2);
-			for(i=0;i<len;i++) a[i]=a[i]*inv%p;
+			int inv=pow2(len,p-2);
+			for(i=0;i<len;i++) a[i]=1ll*a[i]*inv%p;
 		}
 	}
-	//½á¹û´æÔÚa 
-	void mul(ll *a,ll *b,int l1,int l2)
+	void qpow(VI &a,int b)//limt: sz(a)*b is small
 	{
-		int len,i;
+		int len,i,l1;
+		l1=sz(a);
+		for(len=1;len<=(l1+1)*b-1;len<<=1);
+		a.resize(len+1);
+		for(i=l1;i<=len;i++) a[i]=0;
+		ntt(a,len,0);
+		for(i=0;i<len;i++) a[i]=pow2(a[i],b);
+		ntt(a,len,1);
+		a.resize((l1+1)*b-1);
+	}
+	void mul(VI &res,VI a,VI b)
+	{
+		int len,i,l1,l2;
+		l1=sz(a);
+		l2=sz(b);
 		for(len=1;len<=l1+l2;len<<=1);
+		a.resize(len+1);
+		b.resize(len+1);
 		for(i=l1+1;i<=len;i++) a[i]=0;
 		for(i=l2+1;i<=len;i++) b[i]=0;
 		ntt(a,len,0);ntt(b,len,0);
-		for(i=0;i<len;i++) a[i]=a[i]*b[i]%p;
-		ntt(a,len,1);
+		res.resize(len);
+		for(i=0;i<len;i++) res[i]=1ll*a[i]*b[i]%p;
+		ntt(res,len,1);
+		res.resize(l1);
 	}
 };//NTT::getwn();
