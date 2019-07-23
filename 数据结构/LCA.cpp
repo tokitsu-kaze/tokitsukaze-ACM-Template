425ddb8aa46ab099435cbc5fd7620c28bd47d4f2
tokitsu-kaze <861794979@qq.com>
2019-07-23 20:25:53 +0800

    Update LCA.cpp

--- 数据结构/LCA.cpp
+++ 数据结构/LCA.cpp
@@ -1,61 +1,98 @@
-//dfs+ST O(nlogn)‘§¥¶¿Ì O(1)≤È—Ø 
-struct node{int to;int w;node(){}node(int _to,int _w):to(_to),w(_w){}};
-int dis[MAX];
-int path[2*MAX],deep[2*MAX],first[MAX],tot;
-int dp[2*MAX][26];
-vector<node> mp[MAX];
-void dfs(int x,int pre,int h)
+struct LCA
 {
-	int i;
-	path[++tot]=x;
-	first[x]=tot;
-	deep[tot]=h;
-	for(i=0;i<mp[x].size();i++)
+	#define type int
+	struct node{int to;type w;node(){}node(int _to,type _w):to(_to),w(_w){}};
+	type dis[MAX];
+	int path[2*MAX],deep[2*MAX],first[MAX],len[MAX],tot,n;
+	int dp[2*MAX][22];
+	vector<node> mp[MAX];
+	void dfs(int x,int pre,int h)
 	{
-		int to=mp[x][i].to;
-		if(to==pre) continue;
-		dis[to]=dis[x]+mp[x][i].w;
-		dfs(to,x,h+1);
+		int i;
 		path[++tot]=x;
+		first[x]=tot;
 		deep[tot]=h;
-	}
-}
-void ST(int n)
-{
-	int i,j,x,y;
-	for(i=1;i<=n;i++) dp[i][0]=i;
-	for(j=1;(1<<j)<=n;j++)
-	{
-		for(i=1;i+(1<<j)-1<=n;i++)
+		for(i=0;i<mp[x].size();i++)
 		{
-			x=dp[i][j-1];
-			y=dp[i+(1<<(j-1))][j-1];
-			dp[i][j]=deep[x]<deep[y]?x:y;
+			int to=mp[x][i].to;
+			if(to==pre) continue;
+			dis[to]=dis[x]+mp[x][i].w;
+			len[to]=len[x]+1;
+			dfs(to,x,h+1);
+			path[++tot]=x;
+			deep[tot]=h;
 		}
 	}
-}
-int query(int l,int r)
-{
-	int len,x,y;
-	len=(int)log2(r-l+1); 
-	x=dp[l][len];
-	y=dp[r-(1<<len)+1][len];
-	return deep[x]<deep[y]?x:y;
-}
-int LCA(int x,int y)
-{
-	int l,r,pos;
-	l=first[x];
-	r=first[y];
-	if(l>r) swap(l,r);
-	pos=query(l,r);
-	return path[pos];
-} 
-int getdis(int a,int b){return dis[a]+dis[b]-2*dis[LCA(a,b)];}
-void work(int n)
-{
-	for(int i=0;i<=n;i++) dis[i]=0;
-	tot=0;
-	dfs(1,0,0);
-	ST(2*n-1);
-}
+	void ST(int n)
+	{
+		int i,j,x,y;
+		for(i=1;i<=n;i++) dp[i][0]=i;
+		for(j=1;(1<<j)<=n;j++)
+		{
+			for(i=1;i+(1<<j)-1<=n;i++)
+			{
+				x=dp[i][j-1];
+				y=dp[i+(1<<(j-1))][j-1];
+				dp[i][j]=deep[x]<deep[y]?x:y;
+			}
+		}
+	}
+	int query(int l,int r)
+	{
+		int len,x,y;
+		len=(int)log2(r-l+1); 
+		x=dp[l][len];
+		y=dp[r-(1<<len)+1][len];
+		return deep[x]<deep[y]?x:y;
+	}
+	int lca(int x,int y)
+	{
+		int l,r,pos;
+		l=first[x];
+		r=first[y];
+		if(l>r) swap(l,r);
+		pos=query(l,r);
+		return path[pos];
+	} 
+	type get_dis(int a,int b){return dis[a]+dis[b]-2*dis[lca(a,b)];}
+	int get_len(int a,int b){return len[a]+len[b]-2*len[lca(a,b)];}
+	void init(int _n)
+	{
+		n=_n;
+		for(int i=0;i<=n;i++)
+		{
+			dis[i]=0;
+			len[i]=0;
+			mp[i].clear();
+		}
+	}
+	void add_edge(int a,int b,type w=1)
+	{
+		mp[a].pb(node(b,w));
+		mp[b].pb(node(a,w));
+	}
+	void work(int rt)
+	{
+		tot=0;
+		dfs(rt,0,0);
+		ST(2*n-1);
+	}
+	int lca_root(int rt,int a,int b)
+	{
+		int fa,fb;
+		fa=lca(a,rt);
+		fb=lca(b,rt);
+		if(fa==fb) return lca(a,b);
+		else
+		{
+			if(get_dis(fa,rt)<get_dis(fb,rt)) return fa;
+			else return fb;
+		}
+	}
+	#undef type
+}lca;
+/*
+lca.init(n);
+lca.add_edge(a,b,w) undirected edge.
+lca.work(rt);
+*/
