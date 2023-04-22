fdc68549442777df138aadb74ea8e10870c906e3
tokitsukaze <861794979@qq.com>
2023-04-22 09:49:01 +0800

    Update LIS.cpp

--- c++/dp/LIS.cpp
+++ c++/dp/LIS.cpp
@@ -1,35 +1,27 @@
-VI LIS(VI a)
+vector<int> LIS(vector<int> a)
 {
 	int i,pos,len,mx,n;
-	n=sz(a);
-	VI b(n),tmp(n),res;
-	for(i=0;i<n;i++)
-	{
-		//LIS: INF
-		//LDS: -INF
-		b[i]=INF;
-	}
+	n=a.size();
+	vector<int> b(n,INF),tmp(n),res;
 	for(i=0;i<n;i++)
 	{
 		//    strict: lower_bound
 		//not strict: upper_bound
-		pos=lower_bound(all(b),a[i])-b.begin();
+		pos=lower_bound(b.begin(),b.end(),a[i])-b.begin();
 		b[pos]=a[i];
 		tmp[i]=pos;
 	}
-	len=lower_bound(all(b),INF)-b.begin();
+	len=lower_bound(b.begin(),b.end(),INF)-b.begin();
 	mx=INF;
 	for(i=n-1;~i;i--)
 	{
 		if(!len) break;
 		//    strict LIS: >
 		//not strict LIS: >=
-		//    strict LDS: <
-		//not strict LDS: <=
 		if(tmp[i]+1==len&&mx>a[i])
 		{
 			len--;
-			res.pb(i);
+			res.push_back(i);
 			mx=a[i];
 		}
 	}
