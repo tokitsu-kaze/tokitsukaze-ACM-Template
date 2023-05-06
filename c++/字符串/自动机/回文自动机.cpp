f5fb56e008e48a5b283cbc156d5e4ab386be2ac4
tokitsukaze <861794979@qq.com>
2023-05-06 15:33:15 +0800

    Update 回文自动机.cpp

--- c++/字符串/自动机/回文自动机.cpp
+++ c++/字符串/自动机/回文自动机.cpp
@@ -1,10 +1,13 @@
 struct Palindrome_Tree
 {
-	int len[MAX],nex[MAX][26],fail[MAX],last,s[MAX],tot,n;
-	int cnt[MAX],deep[MAX];
+	static const int N=MAX;
+	static const int LOGN=log2(N)+2;
+	static const int K=26;// char size: [0,25]
+	int len[N],nex[N][K],fail[N],last,pos[N],s[N],tot,n;
+	int cnt[N],deep[N];
 	int newnode(int l)
 	{
-		mem(nex[tot],0);
+		memset(nex[tot],0,sizeof nex[tot]);
 		fail[tot]=0; 
 		deep[tot]=cnt[tot]=0;
 		len[tot]=l;
@@ -23,7 +26,7 @@ struct Palindrome_Tree
 		while(s[n-len[x]-1]!=s[n]) x=fail[x];
 		return x;
 	}
-	void add(int t)//attention the type of t is int
+	void add(int t,int p)//attention the type of t is int
 	{
 		int id,now;
 		s[++n]=t;
@@ -37,14 +40,40 @@ struct Palindrome_Tree
 		}
 		last=nex[now][t];
 		cnt[last]++;
+		pos[p]=last;
 	}
 	void count()
 	{
 		for(int i=tot-1;i;i--) cnt[fail[i]]+=cnt[i];
+	}
+	int st[N][LOGN];
+	void init_ST()
+	{
+		int i,j;
+		for(i=2;i<tot;i++)
+		{
+			st[i][0]=fail[i];
+			for(j=1;j<LOGN;j++)
+			{
+				st[i][j]=st[st[i][j-1]][j-1];
+			}
+		}
+	}
+	int get_substr(int l,int r)//init_ST
+	{
+		int now,tmp,i;
+		now=pos[r];
+		for(i=LOGN-1;~i;i--)
+		{
+			if(len[st[now][i]]>=r-l+1) now=st[now][i];
+		}
+		//maybe need judge if len[now]==r-l+1
+		return now;
 	}
 	void build_tree(VI mp[])// root is 0
 	{
 		for(int i=0;i<=tot+1;i++) mp[i].clear();
 		for(int i=1;i<tot;i++) mp[fail[i]].pb(i);
 	}
+	
 }pam; //pam.init(); 
