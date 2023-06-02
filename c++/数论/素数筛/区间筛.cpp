2277d1d432116c3075cd4034e451f42d8efd6109
tokitsukaze <861794979@qq.com>
2023-06-02 20:13:35 +0800

    Update 区间筛.cpp

--- c++/数论/素数筛/区间筛.cpp
+++ c++/数论/素数筛/区间筛.cpp
@@ -1,18 +1,19 @@
 //O(r-l+1)
-ll p[MAX],tot;
-bool flag[MAX],prime[MAX];
+const int N=2e7+10;
+ll p[N],tot;
+bool vis[N],prime[N];
 void init(ll l,ll r)
 {
 	ll i,j,sq=sqrt(r+0.5);
 	tot=0;
-	for(i=0;i<=sq;i++) flag[i]=1;
+	for(i=0;i<=sq;i++) vis[i]=1;
 	for(i=l;i<=r;i++) prime[i-l]=1;
 	if(l==0) prime[0]=prime[1]=0;
 	if(l==1) prime[0]=0;
 	for(i=2;i<=sq;i++)
 	{
-		if(!flag[i]) continue;
-		for(j=i+i;j<=sq;j+=i) flag[j]=0;
+		if(!vis[i]) continue;
+		for(j=i+i;j<=sq;j+=i) vis[j]=0;
 		for(j=max(2LL,(l+i-1)/i)*i;j<=r;j+=i) prime[j-l]=0;
 	}
 	for(i=l;i<=r;i++)
