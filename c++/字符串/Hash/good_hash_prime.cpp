b684e8207d4115779ebae715997b5753a1fca972
tokitsukaze <861794979@qq.com>
2023-06-15 09:32:48 +0800

    Update good_hash_prime.cpp

--- c++/字符串/Hash/good_hash_prime.cpp
+++ c++/字符串/Hash/good_hash_prime.cpp
@@ -1,8 +1,8 @@
-VL good_hash_prime={50331653,100663319,201326611,402653189,805306457,1610612741};
-VL get_hash_prime(int cnt=2)
+vector<ll> good_hash_prime={50331653,100663319,201326611,402653189,805306457,1610612741};
+vector<ll> get_hash_prime(int cnt=2)
 {
-	assert(cnt<=sz(good_hash_prime));
+	assert(cnt<=good_hash_prime.size());
 	srand(time(0));
-	random_shuffle(all(good_hash_prime));
-	return VL(good_hash_prime.begin(),good_hash_prime.begin()+cnt);
+	random_shuffle(good_hash_prime.begin(),good_hash_prime.end());
+	return vector<ll>(good_hash_prime.begin(),good_hash_prime.begin()+cnt);
 }
