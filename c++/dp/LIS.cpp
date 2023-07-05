75f0a1c32d8781da90429a18de351e89345a4a0d
tokitsukaze <861794979@qq.com>
2023-07-05 16:06:18 +0800

    Update LIS.cpp

--- c++/dp/LIS.cpp
+++ c++/dp/LIS.cpp
@@ -1,26 +1,33 @@
-vector<int> LIS(vector<int> a,bool strict)
+struct LIS
 {
-	int i,pos,len,n;
-	n=a.size();
-	vector<int> b(n,INF),tmp(n),res;
-	for(i=0;i<n;i++)
+	#define type int
+	const type inf=INF;
+	vector<int> work(vector<type> a,bool strict)
 	{
-		//    strict: lower_bound
-		//not strict: upper_bound
-		if(strict) pos=lower_bound(b.begin(),b.end(),a[i])-b.begin();
-		else pos=upper_bound(b.begin(),b.end(),a[i])-b.begin();
-		b[pos]=a[i];
-		tmp[i]=pos;
-	}
-	len=lower_bound(b.begin(),b.end(),INF)-b.begin();
-	for(i=n-1;~i;i--)
-	{
-		if(!len) break;
-		if(tmp[i]+1==len)
+		int i,pos,len,n;
+		n=a.size();
+		vector<type> b(n,inf);
+		vector<int> tmp(n),res;
+		for(i=0;i<n;i++)
 		{
-			len--;
-			res.push_back(i);
+			//    strict: lower_bound
+			//not strict: upper_bound
+			if(strict) pos=lower_bound(b.begin(),b.end(),a[i])-b.begin();
+			else pos=upper_bound(b.begin(),b.end(),a[i])-b.begin();
+			b[pos]=a[i];
+			tmp[i]=pos;
 		}
+		len=lower_bound(b.begin(),b.end(),inf)-b.begin();
+		for(i=n-1;~i;i--)
+		{
+			if(!len) break;
+			if(tmp[i]+1==len)
+			{
+				len--;
+				res.push_back(i);
+			}
+		}
+		return res;
 	}
-	return res;
-}
+	#undef type
+}lis;
