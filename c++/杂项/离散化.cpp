cc83a1e5b74cb0c25d222c655cf1fc0fdd50168e
tokitsukaze <861794979@qq.com>
2023-07-05 16:06:46 +0800

    Update 离散化.cpp

--- c++/杂项/离散化.cpp
+++ c++/杂项/离散化.cpp
@@ -1,12 +1,14 @@
 struct Discretization
 {
 	#define type ll
+	#define all(x) x.begin(),x.end()
 	vector<type> a;
-	void init(){a.clear();}
-	void add(type x){a.pb(x);}
+	void clear(){a.clear();}
+	void add(type x){a.push_back(x);}
 	void work(){sort(all(a));a.resize(unique(all(a))-a.begin());}
 	int get_pos(type x){return lower_bound(all(a),x)-a.begin()+1;}
 	type get_val(int pos){return a[pos-1];}
 	int size(){return a.size();}
 	#undef type
+	#undef all
 }d;
