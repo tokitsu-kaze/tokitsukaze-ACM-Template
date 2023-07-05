8c03a2d7f5d571d9997664255e347e517d92fa20
tokitsukaze <861794979@qq.com>
2023-07-05 16:06:31 +0800

    Update AC自动机.cpp

--- c++/字符串/自动机/AC自动机.cpp
+++ c++/字符串/自动机/AC自动机.cpp
@@ -1,12 +1,12 @@
 struct AC_Automaton
 {
-	static const int K=26;//may need change
+	static const int K=;
 	int nex[MAX][K],fail[MAX],cnt[MAX],last[MAX];
-	int root,tot;
-	inline int getid(char c){return c-'a';}//may need change
+	int root,tot,pos[MAX];
+	int getid(char c){return c-;}//may need change
 	int newnode()
 	{
-		mem(nex[tot],0);
+		memset(nex[tot],0,sizeof nex[tot]);
 		fail[tot]=0;
 		cnt[tot]=0;
 		return tot++;
@@ -16,23 +16,23 @@ struct AC_Automaton
 		tot=0;
 		root=newnode();
 	}
-	void insert(char *s)
+	void insert(char *s,int n,int x) // s[0..n-1]
 	{
-		int len,now,i;
-		len=strlen(s);
+		int now,i,t;
 		now=root;
-		for(i=0;i<len;i++)
+		for(i=0;i<n;i++)
 		{
-			int t=getid(s[i]);
+			t=getid(s[i]);
 			if(!nex[now][t]) nex[now][t]=newnode();
 			now=nex[now][t];
 		}
 		cnt[now]++;
+		pos[x]=now;
 	}
 	void work()
 	{
 		int i,now;
-		queue<int>q;
+		queue<int> q;
 		for(i=0;i<K;i++)
 		{
 			if(nex[root][i]) q.push(nex[root][i]);
@@ -41,13 +41,11 @@ struct AC_Automaton
 		{
 			now=q.front();
 			q.pop();
+			
 			//suffix link
 			if(cnt[fail[now]]) last[now]=fail[now];
 			else last[now]=last[fail[now]];
-			/*
-			may need add something here:
-			cnt[now]+=cnt[fail[now]];
-			*/
+			
 			for(i=0;i<K;i++)
 			{
 				if(nex[now][i])
@@ -59,31 +57,61 @@ struct AC_Automaton
 			}
 		}
 	}
-	int query(char *s)
+	int out[MAX];
+	void topsort()
 	{
-		int len,now,i,res;
-		len=strlen(s);
+		int i,t;
+		queue<int> q;
+		for(i=1;i<tot;i++) out[fail[i]]++;
+		for(i=1;i<tot;i++)
+		{
+			if(!out[i]) q.push(i);
+		}
+		while(!q.empty())
+		{
+			t=q.front();
+			q.pop();
+			
+			// do something 
+			
+			out[fail[t]]--;
+			if(out[fail[t]]==0) q.push(fail[t]);
+		}
+	}
+	int query(char *s,int n)
+	{
+		int len,now,i,res,t,tmp;
 		now=root;
 		res=0;
-		for(i=0;i<len;i++)
+		vector<pair<int,int>> del;
+		for(i=0;i<n;i++)
 		{
-			int t=getid(s[i]);
+			t=getid(s[i]);
 			now=nex[now][t];
-			int tmp=now;
+			tmp=now;
 			while(tmp&&cnt[tmp]!=-1)
 			{
 				res+=cnt[tmp];
+				del.push_back({tmp,cnt[tmp]});
 				cnt[tmp]=-1;
 				tmp=last[tmp];
 			}
 		}
+		for(auto &it:del) cnt[it.first]=it.second;
 		return res;
 	}
-	//build fail tree 
-	vector<int> mp[MAX];
-	void build_tree()
+	void build_fail_tree(vector<int> mp[])
 	{
 		for(int i=0;i<=tot;i++) mp[i].clear();
-		for(int i=1;i<tot;i++) mp[fail[i]].pb(i);
+		for(int i=1;i<tot;i++) mp[fail[i]].push_back(i);
 	}
 }ac;
+/*
+i is the suffix for each node in the subtree(i) of the fail tree
+
+ac.init();
+ac.insert(s,len,id); s[0..len-1], id:1..n
+ac.work();
+ac.query(s,len); s[0..len-1]
+ac.build_fail_tree(mp);
+*/
