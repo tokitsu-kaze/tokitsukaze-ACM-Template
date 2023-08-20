ee0953cd6c6462e691940d04f1a4b6d347d9ca6c
tokitsukaze <861794979@qq.com>
2023-08-20 10:17:19 +0800

    Create kruskal重构树.cpp

---
+++ c++/图论/最小生成树/kruskal重构树.cpp
@@ -0,0 +1,64 @@
+struct Disjoint_Set_Union
+{
+	int pre[MAX];
+	void init(int n)
+	{
+		int i;
+		for(i=1;i<=n;i++) pre[i]=i;
+	}
+	int find(int x)
+	{
+		if(pre[x]!=x) pre[x]=find(pre[x]);
+		return pre[x];
+	}
+	bool merge(int a,int b)
+	{
+		int ra,rb;
+		ra=find(a);
+		rb=find(b);
+		if(ra!=rb)
+		{
+			pre[ra]=rb;
+			return 1;
+		}
+		return 0;
+	}
+}dsu;
+struct Kruskal_Tree
+{
+	#define type int
+	#define inf INF
+	struct edge{int x,y;type w;};
+	vector<edge> e;
+	void init(){e.clear();}
+	void add_edge(int x,int y,type w){e.push_back({x,y,w});}
+	int build_kruskal_tree(int n,vector<int> *mp,type *w)
+	{
+		int rt,x,y,i;
+		for(i=1;i<=2*n-1;i++)
+		{
+			mp[i].clear();
+			w[i]=0;
+		}
+		dsu.init(2*n-1);
+		sort(e.begin(),e.end(),[&](edge x,edge y){
+			return x.w<y.w;
+		});
+		rt=n;
+		for(auto &it:e)
+		{
+			x=dsu.find(it.x);
+			y=dsu.find(it.y);
+			if(x==y) continue;
+			rt++;
+			w[rt]=it.w;
+			mp[rt].push_back(x);
+			mp[rt].push_back(y);
+			dsu.merge(x,rt);
+			dsu.merge(y,rt);
+		}
+		return rt;
+	}
+	#undef type
+	#undef inf
+}krsk;
