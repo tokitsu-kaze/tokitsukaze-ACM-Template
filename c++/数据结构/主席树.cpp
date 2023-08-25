343845061a0608e22d3294dd8c44cb624a59a76d
tokitsukaze <861794979@qq.com>
2023-08-25 11:28:09 +0800

    Update 主席树.cpp

--- c++/数据结构/主席树.cpp
+++ c++/数据结构/主席树.cpp
@@ -1,54 +1,52 @@
-struct president_tree
+struct President_Tree
 {
 	#define type int
-	int root[MAX],ls[40*MAX],rs[40*MAX],tot,ql,qr;
-	type sum[40*MAX],qv;
-	void init()
+	int root[MAX],ls[MAX<<5],rs[MAX<<5],tot,ql,qr,n;
+	type v[MAX<<5],tag[MAX<<5],qv;
+	void init(int _n)
 	{
-		mem(root,0);
-		tot=1;
-		ls[0]=rs[0]=sum[0]=0;
+		n=_n;
+		ls[0]=rs[0]=v[0]=tag[0]=tot=0;
 	}
-	int newnode(int x)
+	int copy_node(int x)
 	{
+		tot++;
 		ls[tot]=ls[x];
 		rs[tot]=rs[x];
-		sum[tot]=sum[x];
-		return tot++;
+		v[tot]=v[x];
+		tag[tot]=v[x];
+		return tot;
 	}
-	void insert(int l,int r,int &id,int pre) //set(ql,ql,v)
+	void update(int l,int r,int &id,int pre)
 	{
-		id=newnode(pre);
-		sum[id]+=qv;
-		if(l==r) return;
+		if(!id) id=copy_node(pre);
+		v[id]+=qv*(min(r,qr)-max(l,ql)+1);
+		if(l>=ql&&r<=qr)
+		{
+			tag[id]+=qv;
+			return;
+		}
 		int mid=(l+r)>>1;
-		if(ql<=mid) insert(l,mid,ls[id],ls[pre]);
-		else insert(mid+1,r,rs[id],rs[pre]);
+		if(ql<=mid) update(l,mid,ls[id],ls[pre]);
+		if(qr>mid) update(mid+1,r,rs[id],rs[pre]);
 	}
-	int kindcnt(int l,int r,int id) //set(ql,qr)
+	void create_ver(int now_ver,int pre_ver,int l,int r,type v)
 	{
-		if(ql<=l&&r<=qr) return sum[id]; 
-		int mid=(l+r)>>1;
-		int res=0;
-		if(ql<=mid) res+=kindcnt(l,mid,ls[id]);
-		if(qr>=mid+1) res+=kindcnt(mid+1,r,rs[id]);
-		return res;
+		root[now_ver]=0;
+		upd_ver(now_ver,pre_ver,l,r,v);
 	}
-	int kthsmall(int l,int r,int id,int pre,int k)
+	void update_ver(int now_ver,int pre_ver,int l,int r,type v)
 	{
-		if(l==r) return l;
-		int mid=(l+r)>>1;
-		int temp=sum[ls[id]]-sum[ls[pre]];
-		if(temp>=k) return kthsmall(l,mid,ls[id],ls[pre],k);
-		else return kthsmall(mid+1,r,rs[id],rs[pre],k-temp);
+		if(l>r) return;
+		ql=l;
+		qr=r;
+		qv=v;
+		update(1,n,root[now_ver],root[pre_ver]);
 	}
-	int kthbig(int l,int r,int id,int pre,int k)
-	{
-		if(l==r) return l;
-		int mid=(l+r)>>1;
-		int temp=sum[rs[id]]-sum[rs[pre]];
-		if(temp>=k) return kthbig(mid+1,r,rs[id],rs[pre],k);
-		else return kthbig(l,mid,ls[id],ls[pre],k-temp);
-	}
-	void set(int l,int r,type v=0){ql=l;qr=r;qv=v;}
-}pt;
+}tr;
+/*
+tr.init(n);
+tr.create_ver(now_ver,pre_ver,l,r,v);
+tr.update_ver(now_ver,pre_ver,l,r,v);
+*/
+
