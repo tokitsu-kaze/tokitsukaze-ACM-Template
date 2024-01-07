a523aa344c78634e5da5a3eeacae7a41ed61023d
tokitsukaze <861794979@qq.com>
2024-01-07 18:50:20 +0800

    Update 莫队算法.cpp

--- c++/数据结构/莫队算法.cpp
+++ c++/数据结构/莫队算法.cpp
@@ -1,9 +1,9 @@
 struct MO_Algorithm
 {
 	#define type int
-	struct query_info{int id,l,r;type v;};
+	struct query_info{int l,r,id;type v;};
 	vector<query_info> qst;
-	int n,q;
+	int n,q,a[MAX];
 	type ans[MAX],res;
 	void init(int _n,int _q)
 	{
@@ -11,37 +11,43 @@ struct MO_Algorithm
 		n=_n;
 		q=_q;
 	}
-	void add_query(int id,int l,int r,int v=0) {qst.pb(query_info{id,l,r,v});}
-	int a[MAX];
-	void add(int x,int k)
+	void add_qst(int l,int r,int id,type v=0){qst.push_back({l,r,id,v});}
+	void add(int x)
 	{
 		
 	}
-	void del(int x,int k)
+	void del(int x)
 	{
 		
 	}
 	void work()
 	{
 		int i,l,r,sq;
-		sq=sqrt(n);
-		sort(all(qst),[&](query_info a,query_info b){
+		sq=sqrt(q);
+		sort(qst.begin(),qst.end(),[&](query_info a,query_info b){
 			if(a.l/sq!=b.l/sq) return a.l/sq<b.l/sq;
-			return a.r<b.r;
+			if((a.l/sq)&1) return a.r>b.r;
+			else return a.r<b.r;
 		});
 		
 		for(i=1;i<=q;i++) ans[i]=0;
 		l=1;
 		r=0;
 		res=0;
-		for(auto q:qst)
+		for(auto &q:qst)
 		{
 			while(l<q.l) del(l++);
 			while(l>q.l) add(--l);
 			while(r<q.r) add(++r);
 			while(r>q.r) del(r--);
-			ans[q.id]+=res;
+			ans[q.id]=res;
 		}
 	}
 	#undef type
-}mo;// mo.init(n,q)
+}mo;
+/*
+O(n*sqrt(q))
+mo.init(n,q);
+mo.add_qst(l,r,id);
+mo.work();
+*/
