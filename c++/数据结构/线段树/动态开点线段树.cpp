c514cfbe0b6eb8d302a8de7afb6e207651dc65eb
tokitsukaze <861794979@qq.com>
2024-01-24 21:52:48 +0800

    Update 动态开点线段树.cpp

--- c++/数据结构/线段树/动态开点线段树.cpp
+++ c++/数据结构/线段树/动态开点线段树.cpp
@@ -1,78 +1,144 @@
-﻿//空间大小是nlogm,n为插入的节点总数,m为区间长度 
 struct Segment_Tree
 {
 	#define type int
-	static const int LOGN=31;
-	int root,tot,ls[MAX*LOGN],rs[MAX*LOGN],ql,qr,n;
-	type v[MAX*LOGN],tag[MAX*LOGN],qv;
-	int newnode()
+	static const int LOG=31;
+	struct node
 	{
-		ls[tot]=rs[tot]=0;
-		v[tot]=0;
-		tag[tot]=-1;
-		return tot++;
-	}
-	void build(int _n)
+		type v;
+		void init(){v=0;}
+	}t[MAX*LOG],null_node;
+	int root[MAX],ls[MAX*LOG],rs[MAX*LOG],rt,tot,qop;
+	int st[MAX*LOG],top;
+	type ql,qr,qv,n,tag[MAX*LOG];
+	Segment_Tree &operator[](const int _rt){this->rt=_rt;return *this;}
+	void init(type _n)
 	{
 		n=_n;
-		tot=0;
-		root=newnode();
+		rt=1;
+		tot=top=0;
+		t[0].init();
+		ls[0]=rs[0]=0;
+		null_node.init();
 	}
-	void pushup(int id)
+	void build(){root[rt]=0;}
+	void delnode(int id){st[top++]=id;}
+	int newnode()
 	{
-		
+		int id;
+		id=top?st[--top]:++tot;
+		t[id].init();
+		ls[id]=rs[id]=0;
+		tag[id]=0;
+		return id;
 	}
-	void pushdown(int id)
+	node merge_node(node x,node y)
 	{
-		if(tag[id]==-1) return;
+		node res;
+		//
+		return res;
+	}
+	void pushup(int id){t[id]=merge_node(t[ls[id]],t[rs[id]]);}
+	void pushdown(type l,type r,int id)
+	{
+		if(!tag[id]) return;
 		if(!ls[id]) ls[id]=newnode();
 		if(!rs[id]) rs[id]=newnode();
 		
 		
-		tag[id]=-1;
+		tag[id]=0;
 	}
-	void update(int l,int r,int &id)
+	void update(type l,type r,int &id)
 	{
 		if(!id) id=newnode();
 		if(l>=ql&&r<=qr)
 		{
-			//do something
+			//
 			return;
 		}
-		pushdown(id);
-		int mid=(l+r)>>1;
+		pushdown(l,r,id);
+		type mid=(l+r)>>1;
 		if(ql<=mid) update(l,mid,ls[id]);
 		if(qr>mid) update(mid+1,r,rs[id]);
 		pushup(id);
 	}
-	type res;
-	void query(int l,int r,int &id)
+	node query(type l,type r,int id)
 	{
-		if(!id) return;
+		if(!id) return null_node;
+		if(l>=ql&&r<=qr) return t[id];
+		pushdown(l,r,id);
+		type mid=(l+r)>>1;
+		if(qr<=mid) return query(l,mid,ls[id]);
+		if(ql>mid) return query(mid+1,r,rs[id]);
+		return merge_node(query(l,mid,ls[id]),query(mid+1,r,rs[id]));
+	}
+	int split(type l,type r,int &id)
+	{
+		int x;
+		if(!id) return 0;
 		if(l>=ql&&r<=qr)
 		{
-			//do something
-			return;
+			x=id;
+			id=0;
+			return x;
 		}
-		pushdown(id);
+		x=newnode();
 		int mid=(l+r)>>1;
-		if(ql<=mid) query(l,mid,ls[id]);
-		if(qr>mid) query(mid+1,r,rs[id]);
+		if(ql<=mid) ls[x]=split(l,mid,ls[id]);
+		if(qr>mid) rs[x]=split(mid+1,r,rs[id]);
+		pushup(x);
+		pushup(id);
+		return x;
 	}
-	void upd(int l,int r,type v)
+	int merge(type l,type r,int x,int y)
+	{
+		if(!x||!y) return x+y;
+		if(l==r)
+		{
+			//
+			delnode(y);
+			return x;
+		}
+		type mid=(l+r)>>1;
+		ls[x]=merge(l,mid,ls[x],ls[y]);
+		rs[x]=merge(mid+1,r,rs[x],rs[y]);
+		pushup(x);
+		delnode(y);
+		return x;
+	}
+	void split_segtree(type l,type r,int new_tree)
+	{
+		ql=l;
+		qr=r;
+		root[new_tree]=split(1,n,root[rt]);
+	}
+	void merge_segtree(int y)
+	{
+		root[rt]=merge(1,n,root[rt],root[y]);
+		root[y]=0;
+	}
+	void upd(type l,type r,type v)
 	{
 		ql=l;
 		qr=r;
 		qv=v;
-		update(1,n,root);
+		update(1,n,root[rt]);
 	}
-	type ask(int l,int r)//init res
+	type ask(type l,type r)
 	{
 		ql=l;
 		qr=r;
-		res=0;
-		query(1,n,root);
-		return res;
+		return query(1,n,root[rt]).v;
 	}
 	#undef type
-};
+}tr;
+/*
+tr.init(n);
+tr.build();
+tr.upd(l,r,v);
+tr.ask(l,r);
+Segment_Tree::node res=tr.merge_node(nodex,nodey);
+---------
+tr[x].build();
+tr[x].merge_segtree(y);
+tr[x].split_segtree(l,r,new_tree);
+*/
